7a78581a9bfccaed5d79630b947004af0b26fe8e
tone925git <52178732+tone925@users.noreply.github.com>
2019-07-07 23:01:18 -0400

    edit

--- js/arches.js
+++ js/arches.js
@@ -11,18 +11,18 @@ enterBtn.addEventListener("click", function() {
     label="Radius"
     value=(brow/2+Math.pow(width,2)/(8*brow)).toFixed(2)
   }
-  else if (width>0 && brow==0 && radius>0) {
+  else if (width>0 && brow==0 && radius>=width/2) {
     label="Brow"
     value=(radius-Math.sqrt(Math.pow(radius,2)-Math.pow(width/2,2))).toFixed(2)
   }
-  else if (width==0 && brow>0 && radius>0) {
+  else if (width==0 && brow>0 && radius>=brow) {
     label="Width"
     value=(2*Math.sqrt(2*brow*radius-Math.pow(brow,2))).toFixed(2)
   }
   else {
     label="NA"
     value="NA"
-    alert("Enter dimensions in 2 of the 3 fields\n\nIf the width and brow are specified, then the brow must not be more than half the width")
+    alert("Enter dimensions in 2 of the 3 fields\n\nIf the width and brow are specified, then the brow must not be more than half the width\n\nIf the width and radius are specified, then the radius must not be less than half the width\n\nIf the brow and radius are specified, then the radius must not be less than the brow")
   }
 
   var result=document.getElementById("result")
