60c2e1f5f90709786b942444d7fecec42105abba
tone925git <52178732+tone925@users.noreply.github.com>
2019-07-12 22:14:25 -0400

    e

--- js/squaring.js
+++ js/squaring.js
@@ -1,18 +1,17 @@
-var btn = document.getElementById('btn')
-btn.addEventListener('click', function() {
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
 
-  var side1 = document.getElementById('side1').value
-  var side2 = document.getElementById('side2').value
-  var diagonal
+  var side1 = document.getElementById('side1').value;
+  var side2 = document.getElementById('side2').value;
+  var diagonal;
 
-  if (side1>0 && side2>0) {
-    diagonal = Math.sqrt(Math.pow(side1,2)+Math.pow(side2,2)).toFixed(2)
+  if (side1 > 0 && side2 > 0) {
+    diagonal = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2)).toFixed(2);
+  } else {
+    diagonal = 'NA';
+    alert('Enter dimensions in both fields');
   }
-  else {
-    diagonal = 'NA'
-    alert('Enter dimensions in both fields')
-  }
 
-  var result = document.getElementById('result')
-  result.innerHTML = '<tr><td>Diagonal</td><td>'+diagonal+'</td></tr>'
-})
+  var result = document.getElementById('result');
+  result.innerHTML = '<tr><td>Diagonal</td><td>' + diagonal + '</td></tr>';
+});
